d92bfa35e6e3be86b0b732d592e4816f4ec6bc2c
Enrico Hoffmann <dasrick@gmail.com>
2015-12-02 10:36:23 +0100

    add a bunch of scss and more ...

--- web.js
+++ web.js
@@ -2,6 +2,8 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var express = require('express');
 var serve = express();
 serve.use(express.static(__dirname + '/web'));
+serve.use('/files', express.static(__dirname + '/draft/files'));
+serve.use('/fonts', express.static(__dirname + '/draft/fonts'));
 serve.set('port', (process.env.PORT || 5555));
 serve.listen(serve.get('port'), function () {
   console.log('Node app of Hoffmann Group is running at localhost:' + serve.get('port'));
